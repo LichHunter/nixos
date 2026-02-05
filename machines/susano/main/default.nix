d45846d386931e9ee40cf245581521104df7e032
Alexander <alexander0derevianko@gmail.com>
2026-02-05 15:46:50 +0100

    Fix susano-minimal build, switch to unstable

--- machines/susano/main/default.nix
+++ machines/susano/main/default.nix
@@ -59,7 +59,7 @@ in
     channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+    registry = lib.mkForce (lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs);
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
