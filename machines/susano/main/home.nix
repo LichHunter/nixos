b64aa5b312d689f62c4321e10e502ca43ec60723
Alexander <alexander0derevianko@gmail.com>
2026-02-17 18:00:58 +0100

    Add additions for susano machine

--- machines/susano/main/home.nix
+++ machines/susano/main/home.nix
@@ -27,7 +27,18 @@ in {
           sc = "source $HOME/.zshrc";
           psax = "ps ax | grep";
           cp = "rsync -ah --progress";
+          nixos-build = "nixos-rebuild build --flake ~/nixos/#${username}";
+          nixos-test = "sudo nixos-rebuild test --flake ~/nixos/#${username}";
+          nixos-switch = "sudo nixos-rebuild switch --flake ~/nixos/#${username}";
+          nixos-boot = "sudo nixos-rebuild boot --flake ~/nixos/#${username}";
         };
+      };
+
+      addition = {
+        starship.enable = true;
+        oxidise.enable = true;
+        tmux.enable = true;
+        fzf.enable = true;
       };
     };
   };
